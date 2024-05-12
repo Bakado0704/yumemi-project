96922ddd48c1f97b6c371aa4c5b9c83e95b8443b
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-05-12 21:39:15 +0900

    [feature]commonディレクトリのコンポーネントをまとめたindex.tsを実装

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { FlexBox } from "./FlexBox";
+export { Heading } from "./Heading";
+export { Typography } from "./Typography";
+export { LeftBorder } from "./LeftBorder";
+export { LeftBorderTitle } from "./LeftBorderTitle";
