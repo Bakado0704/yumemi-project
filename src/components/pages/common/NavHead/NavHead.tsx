6e308551791b73073a78047b7659fa152679dbda
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-05-12 21:41:06 +0900

    [feature]NavHeadコンポーネントを実装

---
+++ src/components/pages/common/NavHead/NavHead.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const NavHead = () => {
+  return (
+    <Head>
+      <title>総人口推移グラフ</title>
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
+    </Head>
+  );
+};
+
+export default NavHead;
---
+++ src/components/pages/common/NavHead/index.ts
@@ -0,0 +1,1 @@
+export { default as NavHead } from "./NavHead";
