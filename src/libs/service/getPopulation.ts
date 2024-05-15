c2ebc895bc087cb30c90a9c983fb1a2e56411f54
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-05-15 22:45:42 +0900

    [fix]populationをgetする関数をグラフのデータに合うように修正

--- src/libs/service/getPopulation.ts
+++ src/libs/service/getPopulation.ts
@@ -1,21 +1,37 @@
 import { prefectureData } from "@/data/prefectureData";
-import { PopulationType } from "@/types/application/population.types";
+import {
+  GraphLayoutType,
+  PopulationType,
+} from "@/types/application/population.types";
 
 import { fetchPopulationByPrefCode } from "../api/fetchPopulationByPrefCode";
 
 export const getPopulation = async (
-  prefCodes: number[]
-): Promise<PopulationType[]> => {
-  return Promise.all(
-    prefCodes.map(async (prefCode) => {
-      const prefName = prefectureData.find(
-        (prefecture) => prefecture.prefCode === prefCode
-      )?.prefName;
-      const populationData = await fetchPopulationByPrefCode(prefCode);
-      const filteredData = populationData?.data.filter(
-        (data) => data.label === "総人口"
+  prefCodes: number[],
+  sortKey: string
+): Promise<GraphLayoutType[]> => {
+  const years = [1970, 1980, 1990, 2000, 2010, 2020];
+  const sortedCodes = prefCodes.sort((a, b) => a - b);
+
+  const populationDataByCodes: PopulationType[] = await Promise.all(
+    sortedCodes.map(async (prefCode) => {
+      const prefName =
+        prefectureData.find((prefecture) => prefecture.prefCode === prefCode)
+          ?.prefName || "";
+
+      const data = await fetchPopulationByPrefCode(prefCode);
+      const filteredDataBySortKey = data?.data.filter(
+        (data) => data.label === sortKey
       );
-      const prefData = filteredData?.[0].data;
+      const prefData = years.map((year) => {
+        const yearData = filteredDataBySortKey?.[0].data.find(
+          (data) => data.year === year
+        );
+        return {
+          year,
+          value: yearData ? yearData.value : 0,
+        };
+      });
 
       return {
         prefName,
@@ -23,4 +39,18 @@ export const getPopulation = async (
       };
     })
   );
+
+  const graphData: GraphLayoutType[] = years.map((year) => ({ year }));
+
+  populationDataByCodes.forEach((populationData) => {
+    const { prefName, prefData } = populationData;
+    prefData.forEach(({ year, value }) => {
+      const yearEntry = graphData.find((entry) => entry.year === year);
+      if (yearEntry && value !== null) {
+        yearEntry[prefName] = value;
+      }
+    });
+  });
+
+  return graphData;
 };
