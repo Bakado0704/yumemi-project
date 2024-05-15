ddd29fc953830e157ae89f7da965347053a229d5
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-05-15 22:51:45 +0900

    [fix]PrefInputUnitに全体のFlexBoxで囲むようにする

--- src/components/pages/home/PrefInputUnit/PrefInputUnit.tsx
+++ src/components/pages/home/PrefInputUnit/PrefInputUnit.tsx
@@ -1,4 +1,4 @@
-import { FlexBox } from "@/components/common";
+import { FlexBox, LeftBorderTitle } from "@/components/common";
 import { prefectureData } from "@/data/prefectureData";
 
 import InputItem from "./InputItem/InputItem";
@@ -6,18 +6,20 @@ import styles from "./PrefInputUnit.module.scss";
 import { PrefInputProps } from "./PrefInputUnit.types";
 
 const PrefInputUnit = ({ setPrefCodes }: PrefInputProps) => {
-  const prefList = prefectureData;
   return (
-    <FlexBox className={styles.prefContainer}>
-      {prefList.map((prefData) => {
-        return (
-          <InputItem
-            key={prefData.prefCode}
-            pref={prefData}
-            setPrefCodes={setPrefCodes}
-          />
-        );
-      })}
+    <FlexBox flexDirection="column" gap="2rem" className={styles.container}>
+      <LeftBorderTitle title="都道府県" />
+      <FlexBox className={styles.prefContainer}>
+        {prefectureData.map((prefData) => {
+          return (
+            <InputItem
+              key={prefData.prefCode}
+              pref={prefData}
+              setPrefCodes={setPrefCodes}
+            />
+          );
+        })}
+      </FlexBox>
     </FlexBox>
   );
 };
