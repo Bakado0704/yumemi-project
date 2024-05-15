96c55d08cf4eb7dc1657c2ee36467161230f3566
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-05-15 22:52:49 +0900

    [fix]index.page.tsxの処理のカスタムフック化とreturn内を綺麗にする

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
-
-import { FlexBox, LeftBorderTitle } from "@/components/common";
+import { FlexBox } from "@/components/common";
+import { GraphUnit } from "@/components/pages/home/GraphUnit";
 import { PrefInputUnit } from "@/components/pages/home/PrefInputUnit";
-import { getPopulation } from "@/libs/service/getPopulation";
-import { PopulationType } from "@/types/application/population.types";
-
-import styles from "./page.module.scss";
+import { SelectUnit } from "@/components/pages/home/SelectUnit";
+import { usePopulationData } from "@/hooks/home/usePopulationData";
 
 const Home = () => {
-  const [populationData, setPopulationData] = useState<PopulationType[]>();
-  const [prefCodes, setPrefCodes] = useState<number[]>([]);
-
-  useEffect(() => {
-    getPopulation(prefCodes).then(setPopulationData);
-  }, [prefCodes]);
-
-  console.log(populationData);
+  const { populationData, isDataExist, sortKey, setPrefCodes, setSortKey } =
+    usePopulationData();
 
   return (
-    <FlexBox flexDirection="column" gap="2rem">
-      <FlexBox
-        flexDirection="column"
-        gap="2rem"
-        className={styles.prefContainer}
-      >
-        <LeftBorderTitle title="都道府県" />
-        <PrefInputUnit setPrefCodes={setPrefCodes} />
-      </FlexBox>
+    <FlexBox flexDirection="column">
+      <PrefInputUnit setPrefCodes={setPrefCodes} />
+      <SelectUnit sortKey={sortKey} setSortKey={setSortKey} />
+      <GraphUnit isDataExist={isDataExist} populationData={populationData} />
     </FlexBox>
   );
 };
